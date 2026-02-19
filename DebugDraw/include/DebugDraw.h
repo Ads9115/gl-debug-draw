47cfb3636bd318b084146a124ce168d444b4d49c
adars <adarshsen9115@gmail.com>
2026-02-20 01:09:49 +0530

    Project Structure and Test

---
+++ DebugDraw/include/DebugDraw.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <glm/glm.hpp>
+
+namespace DebugDraw
+{
+    void Init();
+    void Shutdown();
+
+    void BeginFrame();
+    void EndFrame(const glm::mat4& viewProj);
+
+    void DrawLine(
+        const glm::vec3& start,
+        const glm::vec3& end,
+        const glm::vec3& color);
+}
--- gl-debug-draw/Main.cpp
+++ gl-debug-draw/Main.cpp
@@ -1,3 +1,11 @@
 #include<iostream>
 #include<glad/glad.h>
 #include<GLFW/glfw3.h>
+#include "DebugDraw.h"
+#include <glm/glm.hpp>
+
+
+
+int main() {
+
+}
